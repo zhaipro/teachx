f396e9970352bd197684ccf80328f159af3eba8a
zhai_pro <xin_soft@foxmail.com>
2018-10-04 15:31:43 +0800

    先清屏

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -1,11 +1,18 @@
 
 org 07c00h                  ; 告诉编译器程序加载到7c00处
 
+call Clear
 mov si, BootMessage
 call print                  ; 调用显示字符串例程
 jmp $                       ; 无限循环
 
 %include "utils.asm"
+
+; 清屏
+Clear:
+    mov ax, 3
+    int 10h
+    ret
 
 BootMessage     db "Hello teachx world!", 00
 
