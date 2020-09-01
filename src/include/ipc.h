17d3c342af2146e9323054c7d2ac33c1c94df33b
lixin <lixin@moviebook.cn>
2020-09-01 14:46:40 +0800

    消息机制

--- src/include/ipc.h
+++ src/include/ipc.h
@@ -3,9 +3,11 @@
 #define _IPC_H_
 
 struct msg_t{
-    int p0;		// param
+    int p0;     // param
     int p1;
     int p2;
 };
+
+#define IPC_SEND    0X01    // 发送消息 or 接收消息
 
 #endif
--- src/kernel/ipc.c
+++ src/kernel/ipc.c
@@ -13,7 +13,9 @@ struct _msg_t{
     struct _msg_t *next;
 };
 
+static struct _msg_t *s_msgs;
 static struct _msg_t *s_free_msgs;
+static struct _msg_t *s_cur_msgs = NULL;
 
 static struct _msg_t* alloc_msg_block()
 {
@@ -22,13 +24,38 @@ static struct _msg_t* alloc_msg_block()
     return new_block;
 }
 
+static void send(int32_t pid, struct msg_t *msg)
+{
+    struct _msg_t *_msg = alloc_msg_block();
+    _msg->msg = *msg;
+}
+
+static void recv(int32_t pid)
+{
+    struct _msg_t *_msg = s_cur_msgs;
+    for(; _msg; _msg=_msg->next)
+    {
+
+    }
+}
+
 // Inter-Process Communication
 __attribute__((regparm(3)))
 int32_t do_ipc(int32_t eax, int32_t edx, int32_t ecx)
 {
-    struct _msg_t *_msg = alloc_msg_block();
-    _msg->msg = *((struct msg_t*)ecx);
-    printk("eax: %d, msg.ps: %d/%d/%d\n", eax, _msg->msg.p0, _msg->msg.p1, _msg->msg.p2);
+    struct _msg_t *_msg = NULL;
+    if(eax >= 0) {      // 发送给指定进程
+        if(edx | IPC_SEND) {    // 发送
+            send(eax, (struct msg_t*)ecx);
+        } else {                // 接收
+            recv(eax);
+        }
+        _msg = alloc_msg_block();
+        _msg->msg = *((struct msg_t*)ecx);
+        printk("eax: %d, msg.ps: %d/%d/%d\n", eax, _msg->msg.p0, _msg->msg.p1, _msg->msg.p2);
+        return 0;
+    }
+    // 小于零表示消息发送给内核
     return do_fork();
 }
 
